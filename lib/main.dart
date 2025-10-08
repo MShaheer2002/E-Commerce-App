64bd0f169f2def253e47c469567a3e03b8e13898
MShaheer2002 <m.shaheershahid12@gmail.com>
2025-10-08 22:50:43 +0500

    added .env

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:e_commerce_app/core/providers/provider_setup.dart';
 import 'package:e_commerce_app/presentation/providers/theme_provider.dart';
 import 'package:e_commerce_app/presentation/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MultiProvider(
     providers: AppProvider.all,
     child: const MyApp(),
